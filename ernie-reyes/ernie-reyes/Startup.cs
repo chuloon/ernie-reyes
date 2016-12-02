7cd9663d2c4c985773a3b3ff0bfd63a73b650b7c
chuloon <ben.ng@outlook.com>
2016-12-02 18:27:18 -0500

    init
    included assets and knockout

---
+++ ernie-reyes/ernie-reyes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ernie_reyes.Startup))]
+namespace ernie_reyes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
